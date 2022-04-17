f93220de4bde4331fa85d62e0f4669db469f10d8
Declan-Combley <declan-combley@protonmail.com>
2022-04-17 23:07:33 +0800

    made the script more intuitive

--- login.sh
+++ login.sh
@@ -2,10 +2,14 @@
 
 clear 
 
-key=your-ssh-key
+key="<your-ssh-key>"
 
 email=$(git config --get user.email)
 name=$(git config --get user.name)
+
+if [ $key == "<your-ssh-key>" ];then
+	echo "You must replace the key variable with your ssh key in the login.sh file"; echo "If you don't know how to create an ssh key just follow this link https://github.com/settings/tokens/new, and select all relevant scopes."; exit
+fi
 
 if [ $email == "" ] || [ $name == "" ];then
 	echo "Your github email: "; read email
