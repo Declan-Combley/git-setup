f915d25d2ca262bcd4a6bf24280c76fbf0869c81
Declan-Combley <declan-combley@protonmail.com>
2022-04-25 16:33:22 +0800

    simplified the script

--- login.sh
+++ login.sh
@@ -1,5 +1,19 @@
 #!/bin/bash
 
+echo -n "Have you set your git username and email? "
+
+read answer
+
+if [ $answer == "no" ] || [ $answer == "No" ]; then
+  echo "use these commands"; echo ""; sleep .5
+
+  echo 'git config --global user.email "your-email"'
+  echo 'git config --global user.name "your-username"'
+  
+  echo ""; echo "do that and then run this script again"; sleep .5
+  exit
+fi
+
 git config --global credential.helper store 
 
 git add .
