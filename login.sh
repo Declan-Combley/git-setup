f02aac01abbb14bab18802142955633f81b5d349
Declan Combley <90402120+Declan-Combley@users.noreply.github.com>
2022-04-25 16:08:01 +0800

    Update login.sh

--- login.sh
+++ login.sh
@@ -1,11 +1,5 @@
 #!/bin/bash
 
-key="<your-ssh-key>"
-
-echo $key | xclip -selection c
-
-echo "Your can now past your ssh key using Ctrl-V"
-
 git config --global credential.helper store 
 
 git add .
