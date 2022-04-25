9a1a4c1967ee5dd9d6b4c10133893d32822d6c75
Declan Combley <90402120+Declan-Combley@users.noreply.github.com>
2022-04-25 16:05:24 +0800

    Update login.sh

--- login.sh
+++ login.sh
@@ -1,25 +1,10 @@
 #!/bin/bash
 
-clear 
-
 key="<your-ssh-key>"
 
-email=$(git config --get user.email)
-name=$(git config --get user.name)
+echo $key | xclip -selection c
 
-if [ $key == "<your-ssh-key>" ]; then
-	echo "You must replace the key variable with your ssh key in the login.sh file"; echo "If you don't know how to create an ssh key just follow this link https://github.com/settings/tokens/new, and select all relevant scopes."; exit
-fi
-
-if [ $email == "" ] || [ $name == "" ]; then
-	read -p "Your github email: " email
-	read -p "Your username: " name
-
-	git config --global user.email "$email"
-	git config --global user.name "$name"
-fi
-
-echo $key | xclip -selection c; echo "Your can now past your ssh key using Ctrl-V"
+echo "Your can now past your ssh key using Ctrl-V"
 
 git config --global credential.helper store 
 
@@ -28,7 +13,3 @@ git add .
 git commit -m "Logging In"
 
 git push
-
-echo "done ..."; sleep 1.5
-
-clear
